64daa210b496198645e9c99330c852ebc33f6965
at1208 <amantiwari12081999@gmail.com>
2020-05-06 17:53:19 +0530

    head added

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,34 @@
 import Layout from '../components/Layout';
 import { withRouter } from 'next/router';
 import SigninComponent from '../components/auth/SigninComponent';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../config';
 
 const Signin = ({ router }) => {
+
+  const head = () => (
+      <Head>
+          <title>Sign In |{APP_NAME}</title>
+          <meta
+              name="description"
+              content="Tech articles on mobiles, laptops, cybersecurity, games and much more."
+          />
+          <link rel="canonical" href={`${DOMAIN}`} />
+          <meta property="og:title" content={`Latest tech articles | ${APP_NAME}`} />
+          <meta
+              property="og:description"
+              content="Tech articles on mobiles, laptops, cybersecurity, games and much more."
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+          <meta property="og:site_name" content={`${APP_NAME}`} />
+          <meta property="og:image" content={`${DOMAIN}/static/images/home-logo.svg`} />
+          <meta property="og:image:secure_url" content={`${DOMAIN}/static/images/home-logo.svg`} />
+          <meta property="og:image:type" content="image/jpg" />
+          <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+      </Head>
+  );
+
+
 
     const showRedirectMessage = () => {
         if (router.query.message) {
@@ -14,6 +40,7 @@ const Signin = ({ router }) => {
 
     return (
       <>
+      {head()}
         <Layout>
            <div className='container'>
              <div className='row col justify-content-center'>
--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,38 @@
 import Layout from '../components/Layout';
 import SignupComponent from '../components/auth/SignupComponent';
 import Link from 'next/link';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../config';
 
 const Signup = () => {
+
+    const head = () => (
+        <Head>
+            <title>Create Account |{APP_NAME}</title>
+            <meta
+                name="description"
+                content="Tech articles on mobiles, laptops, cybersecurity, games and much more."
+            />
+            <link rel="canonical" href={`${DOMAIN}`} />
+            <meta property="og:title" content={`Latest tech articles | ${APP_NAME}`} />
+            <meta
+                property="og:description"
+                content="Tech articles on mobiles, laptops, cybersecurity, games and much more."
+            />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
+            <meta property="og:image" content={`${DOMAIN}/static/images/home-logo.svg`} />
+            <meta property="og:image:secure_url" content={`${DOMAIN}/static/images/home-logo.svg`} />
+            <meta property="og:image:type" content="image/jpg" />
+            <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+        </Head>
+    );
+
+
+
     return (
         <Layout>
+        {head()}
         <div className='row col justify-content-center'>
            <div className='col-md-6 shadow mt-5   sign'>
              <h1 className='text-center'>Create account</h1>
