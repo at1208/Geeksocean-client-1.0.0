ff2b8e2e441bc92dea0543587651c9d1b02e4078
at1208 <amantiwari12081999@gmail.com>
2020-05-20 14:49:30 +0530

    structure changed

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,7 @@ class MyDocument extends Document {
            "logo": "https://geeksocean.com/static/images/Logo.svg",
            "sameAs": [
                 "https://www.facebook.com/Geeksocean-108336497487523/",
+                "https://www.instagram.com/geeksocean/",
                 "https://twitter.com/geeks_ocean",
                 "https://www.linkedin.com/company/geeks-ocean/"
               ]
@@ -61,7 +62,7 @@ render() {
           <link defer rel="preconnect" href="https://www.googletagmanager.com" crossorigin />
           <link defer href="https://fonts.googleapis.com/css?family=Gruppo|Philosopher|Playfair+Display+SC|Spectral+SC|Trirong|Halant:700|Lateef|Lato|Philosopher|Prata|Source+Sans+ProGotu|Marmelad|EB+Garamond|Nunito+Sans:800|Poppins:500&display=swap" rel="stylesheet" />
 
-          <link rel="icon" href="/static/images/Logo.svg" />
+          <link rel="icon" href="/static/images/Logo.jpg" />
           <script defer async src="https://www.googletagmanager.com/gtag/js?id=UA-161776444-1"></script>
           <script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
           <script  defer dangerouslySetInnerHTML={this.setGoogleTags()} />
